e1f0a20d4340adaaaecaa99baffb311c36f24f77
John Lamb <lamb.john.n@gmail.com>
2018-03-07 18:25:37 +0100

    Updating the install script

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 # Set current directory, only run from ~/dotfiles
 dir=~/dotfiles
 
-# Install base16-shell if not already installed (source from .bashrc)
-test -d "~/.config/base16-shell" && git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-
 # Create neovim config folder if not exists
-test -d "~/.config/nvim" && mkdir -p ~/.config/nvim/
+mkdir -p ~/.config/nvim
+
+# Install base16-shell if not already installed (source from .bashrc)
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
 
 # Symlink MacOS .bash_profile
 if [ "$(uname)" == "Darwin" ]; then
