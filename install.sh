0e6f46f4fa69b922fef7abe3847cc660cbff8df3
John Lamb <lamb.john.n@gmail.com>
2018-06-27 21:13:48 +0200

    install script updates

--- install.sh
+++ install.sh
@@ -6,16 +6,19 @@ dir=~/dotfiles
 mkdir -p ~/.config/nvim
 
 # Install base16-shell if not already installed (source from .bashrc)
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+if [ ! -d ~/.config/base16-shell ]; then
+    git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+fi
 
 
 # Symlink MacOS .bash_profile
 if [ "$(uname)" == "Darwin" ]; then
-	ln -sf $dir/bashrc ~/.bash_profile
+    ln -sf $dir/bashrc ~/.bash_profile
 fi
 
 # Always symlink .bashrc
 ln -sf $dir/bashrc ~/.bashrc
 
 ln -sf $dir/nvimrc ~/.config/nvim/init.vim
+ln -sf $dir/ycm_extra_conf.py ~/.config/nvim/.ycm_extra_conf.py
 ln -sf $dir/tmux.conf ~/.tmux.conf
